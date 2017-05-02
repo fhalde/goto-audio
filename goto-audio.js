ac309f59cd85fbb0ba6e553006626bf6efe590ef
Faiz Halde <faiz_84123@yahoo.com>
2017-05-02 18:17:06 +0530

    completed

---
+++ goto-audio.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var last = undefined;
+
+chrome.browserAction.onClicked.addListener(curr => {
+  chrome.tabs.query({}, tabs => {
+    var count = 0;
+    for (var i = 0; i < tabs.length; i++) {
+      if (tabs[i].audible) {
+          count++;
+      }
+    }
+
+    if (count == 0) {
+      last = undefined;
+    }
+
+    for (var i = 0; i < tabs.length; i++) {
+
+      if (tabs[i].audible && count == 1) {
+        chrome.tabs.update(tabs[i].id, { selected: true });
+        last = tabs[i].id;
+        break;
+	    }
+
+      if (tabs[i].audible && last != tabs[i].id) {
+        chrome.tabs.update(tabs[i].id, { selected: true });
+        last = tabs[i].id;
+        break;
+      }
+    }
+  });
+});
